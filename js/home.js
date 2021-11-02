40f999fd3f746e97c6c278ffe063fc02b3348500
Vasu Agrawal <vasu4651@gmail.com>
2021-11-03 02:44:06 +0530

    made items responisve

--- js/home.js
+++ js/home.js
@@ -2,6 +2,11 @@ let sec1 = document.querySelector('.sec1');
 const items = [
    {Name: "Maggi", imgSrc: "https://newbox.in/wp-content/uploads/2021/07/Maggi-easy-peasy-recipe-noodles-70g.jpeg", quantity: "1 x 70g", price: "Rs. 10"},
    {Name: "Bingo Mad Angles", imgSrc: "https://m.media-amazon.com/images/I/81f9gLzqvJL._SL1500_.jpg", quantity: "1 x 72.5g", price: "Rs. 16"},
+   {Name: "Bingo Mad Angles", imgSrc: "https://m.media-amazon.com/images/I/81f9gLzqvJL._SL1500_.jpg", quantity: "1 x 72.5g", price: "Rs. 16"},
+   {Name: "Bingo Mad Angles", imgSrc: "https://m.media-amazon.com/images/I/81f9gLzqvJL._SL1500_.jpg", quantity: "1 x 72.5g", price: "Rs. 16"},
+   {Name: "Bingo Mad Angles", imgSrc: "https://m.media-amazon.com/images/I/81f9gLzqvJL._SL1500_.jpg", quantity: "1 x 72.5g", price: "Rs. 16"},
+   {Name: "Bingo Mad Angles", imgSrc: "https://m.media-amazon.com/images/I/81f9gLzqvJL._SL1500_.jpg", quantity: "1 x 72.5g", price: "Rs. 16"},
+   {Name: "Bingo Mad Angles", imgSrc: "https://m.media-amazon.com/images/I/81f9gLzqvJL._SL1500_.jpg", quantity: "1 x 72.5g", price: "Rs. 16"},
 ];
 
 
